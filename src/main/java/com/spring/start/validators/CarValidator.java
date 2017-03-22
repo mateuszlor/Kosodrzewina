f1c1669e167879bfdcceabb8d171d0bba86f9176
Vertig00 <kluchus@gmail.com>
2017-03-22 22:09:24 +0100

    Car Validator

--- src/main/java/com/spring/start/validators/CarValidator.java
+++ src/main/java/com/spring/start/validators/CarValidator.java
@@ -18,8 +18,8 @@ import org.springframework.validation.Validator;
 @Log4j
 public class CarValidator implements Validator {
 
-    private static final String TEXT_PATTERN = "([a-zA-Z]|[ŁłŻż]){1}([a-z]|[ąęóźćżśł])*";
-    private static final String PHONE_PATTERN = "([0-9]{6,11})";
+    private static final String TEXT_PATTERN = "([a-zA-Z]){1}([a-z])*";
+    private static final String REGISTRATION_NUMBER_PATTERN= "([A-Z]){2,3}(\\s)?([A-Z0-9]){3,5}";
 
     @Autowired
     @Getter @Setter
@@ -33,25 +33,26 @@ public class CarValidator implements Validator {
     @Override
     public void validate(Object o, Errors errors) {
 
-        CarDto customer = (CarDto) o;
-        //TODO: walidacja
-//        ValidationUtils.rejectIfEmpty(errors, "name", environment.getProperty("error.emptyField"));
-//        ValidationUtils.rejectIfEmpty(errors, "surname", environment.getProperty("error.emptyField"));
-//
-//        if (!customer.getName().matches(TEXT_PATTERN)){
-//            errors.rejectValue("name", environment.getProperty("error.invalidText"));
-//            log.warn("Nieprawidłowe imię klienta");
-//        }
-//
-//        if (!customer.getSurname().matches(TEXT_PATTERN)){
-//            errors.rejectValue("surname", environment.getProperty("error.invalidText"));
-//            log.warn("Nieprawidłowe nazwisko klienta");
-//        }
-//
-//        if (!customer.getPhone().matches(PHONE_PATTERN)){
-//            errors.rejectValue("phone", environment.getProperty("error.invalidPhoneNumber"));
-//            log.warn("Nieprawidłowy numer telefonu");
-//        }
+        CarDto car = (CarDto) o;
+
+        ValidationUtils.rejectIfEmpty(errors, "brand", environment.getProperty("error.emptyField"));
+        ValidationUtils.rejectIfEmpty(errors, "model", environment.getProperty("error.emptyField"));
+        ValidationUtils.rejectIfEmpty(errors, "registrationNumber", environment.getProperty("error.emptyField"));
+
+        if (!car.getBrand().matches(TEXT_PATTERN)){
+            errors.rejectValue("brand", environment.getProperty("error.invalidText"));
+            log.warn("Nieprawidłowa marka samochodu");
+        }
+
+        if (!car.getModel().matches(TEXT_PATTERN)){
+            errors.rejectValue("model", environment.getProperty("error.invalidText"));
+            log.warn("Nieprawidłowy model samochodu");
+        }
+
+        if (!car.getRegistrationNumber().matches(REGISTRATION_NUMBER_PATTERN)){
+            errors.rejectValue("registrationNumber", environment.getProperty("error.invalidRegistrationNumber"));
+            log.warn("Nieprawidłowy numer rejestracyjny");
+        }
 
     }
 }
