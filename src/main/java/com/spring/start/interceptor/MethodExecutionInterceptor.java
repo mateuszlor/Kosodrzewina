f2b11b80f13e59e88ccbd14d04e0be798f58380b
Mateusz Lorencki <mateuszlorencki@gmail.com>
2017-03-27 10:08:23 +0200

    Method execution tracking (using AspectJ)

---
+++ src/main/java/com/spring/start/interceptor/MethodExecutionInterceptor.java
@@ -0,0 +1,48 @@
+package com.spring.start.interceptor;
+
+import lombok.experimental.var;
+import lombok.extern.log4j.Log4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ * Created by Mateusz on 27.03.2017.
+ */
+@var
+@Log4j
+@Aspect
+@Component
+public class MethodExecutionInterceptor {
+    @Around("execution(* com.spring.start..*.*(..))")
+    public Object logTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result = null;
+        var isFailed = false;
+        var methodFullName = String.format("%s.%s()", joinPoint.getSignature().getDeclaringType().getCanonicalName(), joinPoint.getSignature().getName());
+
+        var sw = new StopWatch();
+        sw.start();
+
+        log.info(String.format("### Method %s invoked", methodFullName));
+
+        try {
+            result = joinPoint.proceed();
+        }
+        catch (Exception e){
+            isFailed = true;
+            throw e;
+        }
+        finally {
+            sw.stop();
+
+            log.info(String.format("### Execution of method %s finished %s, took %s ms",
+                    methodFullName,
+                    isFailed ? "unsuccessfully" : "successfully",
+                    sw.getTotalTimeMillis()));
+
+            return result;
+        }
+    }
+}
