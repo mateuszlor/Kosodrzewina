06e22f344a22d5831fd7566b3b440aa93987912e
Mateusz Lorencki <mateuszlorencki@gmail.com>
2017-04-13 23:38:04 +0200

    Car jinq repository stub

---
+++ src/main/java/com/spring/start/repository/CarRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.spring.start.repository;
+
+import com.spring.start.entity.Car;
+import lombok.experimental.var;
+
+/**
+ * Created by Mateusz on 13.04.2017.
+ */
+@var
+public class CarRepositoryImpl extends BaseAdditionalRepositoryImpl<Car> implements CarRepositoryAdditional {
+
+}
