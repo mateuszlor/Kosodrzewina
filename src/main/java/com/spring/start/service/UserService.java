a40ef52f22404b9e22eb43942b789bbc8693e13d
Mateusz Lorencki <mateuszlorencki@gmail.com>
2018-01-25 20:38:52 +0100

    Commit fix (imports again)

--- src/main/java/com/spring/start/service/UserService.java
+++ src/main/java/com/spring/start/service/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
