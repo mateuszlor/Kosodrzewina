aab21b711a29e52fa2d178fcee6f0c11dce17d54
Vertig0 <kluchus@gmail.com>
2018-01-31 18:33:50 +0100

    Przekazywanie Enum przez REST i widoczność przycisków na ekranie wyporzyczeń

--- src/main/java/com/spring/start/entity/DictionaryType.java
+++ src/main/java/com/spring/start/entity/DictionaryType.java
@@ -1,24 +1,22 @@
 package com.spring.start.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
  * Created by Vertig0 on 27.03.2017.
  */
 @NoArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@AllArgsConstructor
+@Getter
 public enum DictionaryType {
 
     SERVICE("Serwis"),
     PAYMENT("Okresowe");
 
     private String displayName;
-
-    DictionaryType(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
 
 }
--- src/main/java/com/spring/start/entity/RentStatus.java
+++ src/main/java/com/spring/start/entity/RentStatus.java
@@ -1,8 +1,10 @@
 package com.spring.start.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Created by Vertig0 on 15.04.2017.
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum RentStatus {
 
     RESERVED("Zarezerwowane", 1),
