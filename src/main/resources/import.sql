80532ec4e7c45c074076eecf712be9a1f9816d5a
Vertig00 <kluchus@gmail.com>
2018-03-22 20:23:26 +0100

    Automatyczne dodawanie defaultowego użytkownika na starcie

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into users values(1, 'admin@gmail.com', 1, 'admin', '$2a$10$.4zOJc0JxY0WlIB/Hua1puUclDg2htRZTYi6Tl72YGIR5zFSAi5Ta', 'admin', 'admin');
+insert into user_role values(1, 'ADMIN');
