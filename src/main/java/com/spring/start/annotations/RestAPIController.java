213bd57290b912ee4ed5f3f55842b001c4dcb134
Vertig00 <kluchus@gmail.com>
2018-05-21 14:40:22 +0200

    Adnotacja dla Kontrolerów restowych

---
+++ src/main/java/com/spring/start/annotations/RestAPIController.java
@@ -0,0 +1,18 @@
+package com.spring.start.annotations;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@RestController
+@RequestMapping("/api/")
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RestAPIController {
+}
--- src/main/java/com/spring/start/controller/car/CarRestController.java
+++ src/main/java/com/spring/start/controller/car/CarRestController.java
@@ -1,5 +1,6 @@
 package com.spring.start.controller.car;
 
+import com.spring.start.annotations.RestAPIController;
 import com.spring.start.entity.Car;
 import com.spring.start.service.CarService;
 import lombok.Getter;
@@ -7,26 +8,22 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@RestController
 @Log4j
+@RestAPIController
 public class CarRestController {
     //TODO: jakaś klasa nadrzędna czy coś?
-    private static final String SLASH = "/";
     private static final String GET_CARS= "getCars";
-    private static final String REST = "rest";
 
     @Autowired
     @Getter
     @Setter
     private CarService carService;
 
-    @RequestMapping(value = SLASH + REST + SLASH + GET_CARS, method = RequestMethod.GET)
+    @GetMapping(value = GET_CARS)
     public List<Car> getCarsByRest(Model model) {
         log.info("REST: Pobrano liste samochodów");
         return carService.findAllActive();
